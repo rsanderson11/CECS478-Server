3bfd8587161608caadf3fe67c935d53ab4cbed3c
ry101 <31788821+ry101@users.noreply.github.com>
2018-12-11 11:32:06 -0800

    Changes to app.js

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
 
 var UserController = require('./user/UserController');
-app.use('/api/users', UserController);
+// app.use('/api/users', UserController);
 
 var AuthController = require('./auth/AuthController');
-app.use('/api/auth', AuthController);
+// app.use('/api/auth', AuthController);
 
 var MessageController = require('./message/MessageController');
-app.use('/api/mess', MessageController);
+// app.use('/api/mess', MessageController);
 
 
-// app.get('/', function(req, res) {
-//   res.render('app/auth');
-//   res.render('api/mess');
-//   res.render('api/users');
-// })
+app.get('/', function(req, res) {
+  res.send("Dolittle DA");
+  app.use('app/auth', AuthController);
+  app.use('api/mess', MessageController);
+  app.use('api/users', UserController);
+})
 
 
 
@@ -30,4 +31,4 @@ app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
 
-module.exports = app;
+// module.exports = app;
